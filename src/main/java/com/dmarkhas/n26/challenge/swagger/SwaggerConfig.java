da861185e9179a7adcb670f4d3d67703c6a32651
Dan Markhasin <dan.markhasin@intel.com>
2018-07-20 18:06:44 +0300

    swagger config

--- src/main/java/com/dmarkhas/n26/challenge/swagger/SwaggerConfig.java
+++ src/main/java/com/dmarkhas/n26/challenge/swagger/SwaggerConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,7 +20,20 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo()
+    {
+        return new ApiInfo("N26 Coding Challenge API",
+                "A service for POSTing transactions and generating statistics",
+                "1.0",
+                "",
+                new Contact("Dan Markhasin","",""),
+                "",
+                "",
+                Collections.EMPTY_LIST);
     }
 
 }
